4a856e880ab21dc678245fcc6f80d52e4ce199bf
Russell Greene <russellgreene8@gmail.com>
2018-11-23 15:25:12 -0500

    Put different stations in different lines

--- data-visualizer/src/SensorGraph.tsx
+++ data-visualizer/src/SensorGraph.tsx
@@ -4,10 +4,28 @@ import * as WebRequest from "web-request";
 import { SensorReading } from './SensorData';
 import './SensorTitles';
 import { units, displayName } from './SensorTitles';
+import { red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber } from '@material-ui/core/colors';
 
 interface Props {
     sensor: string;
 }
+
+const chartColors = [
+    red["200"],
+    blue["200"],
+    green["200"],
+    yellow["200"],
+    amber["200"],
+    purple["200"],
+    cyan["200"],
+    teal["200"],
+    lightGreen["200"],
+    indigo["200"],
+    deepPurple["200"],
+    lime["200"],
+    lightBlue["200"],
+    pink["200"],
+];
 
 class SensorGraph extends React.Component<Props> {
 
@@ -24,13 +42,7 @@ class SensorGraph extends React.Component<Props> {
         this.chart = new Chart(this.chartElement, {
             type: 'line',
             data: {
-                datasets: [{
-                    label: `${displayName(this.props.sensor)}`,
-                    data: [],
-                    fill: true,
-                    borderColor: '#ff6961',
-                    backgroundColor: '#db898455'
-                }]
+                datasets: []
             },
             options: {
                 responsive: false,
@@ -85,8 +97,24 @@ class SensorGraph extends React.Component<Props> {
             return;
         }
 
+        // sort the old data based on the station it comes from
+        const datapoints = {};
+        oldData.forEach(d => {
+            if (datapoints[d.station] !== undefined) {
+                datapoints[d.station].push({ x: new Date(d.time * 1000), y: d.reading });
+            } else {
+                datapoints[d.station] = [{ x: new Date(d.time * 1000), y: d.reading }];
+            }
+        });
+
         if (this.chart !== undefined && this.chart.data.datasets !== undefined) {
-            this.chart.data.datasets[0].data = oldData.map(elem => ({ x: new Date(elem.time * 1000), y: elem.reading }));
+            this.chart.data.datasets = Object.keys(datapoints).map((station, idx) => ({
+                label: station,
+                data: datapoints[station],
+                fill: false,
+                borderColor: chartColors[idx % chartColors.length],
+                backgroundColor: chartColors[idx % chartColors.length],
+            }))
             this.chart.update();
         }
 
@@ -105,11 +133,27 @@ class SensorGraph extends React.Component<Props> {
         const datasets = this.chart.data.datasets;
         if (datasets === undefined) { return; }
 
-        const sensorData = datasets[0].data as ChartPoint[];
-        if (sensorData === undefined) { return; }
+        // get the station data
+        let sensorData: ChartPoint[] | undefined;
+        for (const set of datasets) {
+            if (set.label !== undefined && set.label === dataPoint.station) {
+                sensorData = set.data as ChartPoint[]
+            }
+        }
 
-        sensorData.push({ x: new Date(dataPoint.time * 1000), y: dataPoint.reading });
-        this.chart.update();
+        if (sensorData === undefined) {
+            // add the station to the graph
+            datasets.push({
+                label: dataPoint.station,
+                data: [{ x: new Date(dataPoint.time * 1000), y: dataPoint.reading }],
+                fill: false,
+                borderColor: chartColors[datasets.length % chartColors.length],
+                backgroundColor: chartColors[datasets.length % chartColors.length]
+            });
+        } else {
+            sensorData.push({ x: new Date(dataPoint.time * 1000), y: dataPoint.reading });
+            this.chart.update();
+        }
     }
 
 }
